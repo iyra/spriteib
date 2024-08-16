be214502dd8c07b69cc7988b79974379d7d8b291
cccccccccc8 <cccccccccc8@nowhere>
2024-08-16 18:10:23 +0100

    added more

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use poem::{get, handler, listener::TcpListener, web::Path, Route, Server};
+
+#[handler]
+fn hello(Path(name): Path<String>) -> String {
+    format!("hello: {}", name)
+}
+
+#[handler]
+fn get_thread(Path((board, thread)): Path<(String, String)>) -> String {
+    format!("hello: {}", name);
+    
+}
+
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error> {
+    let app = Route::new()
+        .at("/board/:board<\\[A-Za-z]+>/:thread<\\d+>", get(get_thread));
+    Server::new(TcpListener::bind("0.0.0.0:3000"))
+      .run(app)
+      .await
 }

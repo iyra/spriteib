1c8331dd10691703767c3802e60edc43b1a71e47
cccccccccc8 <cccccccccc8@nowhere>
2024-08-27 19:07:13 +0100

    Add redis connection, subscriber to test channel

--- worker/src/main.rs
+++ worker/src/main.rs
@@ -4,24 +4,20 @@ use redis::aio::{MultiplexedConnection, PubSub};
 use redis::{AsyncCommands, Client, ConnectionInfo, RedisError};
 use std::error;
 use serde::ser::{Serialize};
+use serde_json::{json, Value};
+use futures_util::StreamExt as _;
 
 pub struct RedisBus {
     pub uri: String,
-}
-
-pub fn publish_message(conn: &mut Connection, channel: &str, message: &str) -> Result<(), Box<dyn std::error::Error>>
-{
-    //let json = serde_json::to_string(message)?;
-    let p = Cmd::publish(channel, message);
-    p.exec(conn)?;
-    Ok(())
+    connection: Option<MultiplexedConnection>,
 }
 
 impl RedisBus {
-    pub async fn connect(&mut self) -> Result<MultiplexedConnection, RedisError> {
-        let client = redis::Client::open(self.uri.clone()).unwrap();
+    pub async fn connect(&mut self) -> Result<(), RedisError> {
+        let client = redis::Client::open(self.uri.clone()).expect("db wrong");
         let connection = client.get_multiplexed_async_connection().await?;
-        Ok(connection)
+        self.connection = Some(connection);
+        Ok(())
     }
 
     pub async fn pubsub(&mut self) -> Result<PubSub, RedisError> {
@@ -32,16 +28,42 @@ impl RedisBus {
     }
 
     pub async fn publish(&mut self, channel: &str, message: &str) {
-        let ps = &mut self.connect().await;
+        let ps = &mut self.connection;
         match ps {
-            Ok(mp) => {
-                match mp.publish(channel, message.to_string()).await {
-                    Ok(data) => data,
-                    Err(e) => {
-                    } 
-                }
-            }
-            Err(e) => println!("z")
-        }
+            Some(conn) => {
+                match conn.publish(channel, message.to_string()).await {
+                        Ok(data) => data,
+                        Err(e) => {
+                            println!("Error publishing");
+                        } 
+                    }
+            },
+            None => println!("No connection specified")
+        };
     }
+}
+
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error> {
+    let mut bus = RedisBus {
+        uri: "redis://localhost:6379/0".to_string(),
+        connection: None
+    };
+    println!("hhh");
+    bus.connect().await.expect("shit");
+
+
+    let mut ps = bus.pubsub().await.expect("shit!");
+    ps.subscribe("x").await.expect("ns");
+    println!("xxx");
+    while let Some(msg) = ps.on_message().next().await {
+        tokio::task::spawn(async move {
+            // Parse the payload
+            let payload = msg.get_payload::<String>().unwrap();
+            println!("Received message: {}", payload);
+        });
+    }
+
+    bus.publish("x", "{\"h\": 3}").await;
+    Ok(())
 }
